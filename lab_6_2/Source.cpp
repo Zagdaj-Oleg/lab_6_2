969a5600aff208bf00852b8d7d00c1d84e45f2f7
Zagdaj-Oleg <72668217+Zagdaj-Oleg@users.noreply.github.com>
2020-11-16 22:20:11 +0200

    lab_6_2

---
+++ lab_6_2/Source.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+void Create(int* a, const int size, const int Low, const int High, int i)
+{
+	a[i] = Low + rand() % (High - Low + 1);
+	if (i < size - 1)
+		Create(a, size, Low, High, i + 1);
+}
+void Print(int* a, const int size, int i)
+{
+	cout << setw(4) << a[i];
+	if (i < size - 1)
+		Print(a, size, i + 1);
+	else
+		cout << endl;
+}
+void Inverse(int* a, const int n, int i)
+{
+	int tmp = a[i];
+	a[i] = a[n - 1 - i];
+	a[n - 1 - i] = tmp;
+	if (i < n / 2 - 1)
+		Inverse(a, n, i + 1);
+}
+int main()
+{
+	srand((unsigned)time(NULL));
+	const int n = 24;
+	int a[n];
+	int Low = -15;
+	int High = 75;
+	Create(a, n, Low, High, 0);
+	Print(a, n, 0);
+	Inverse(a, n, 0);
+	Print(a, n, 0);
+	return 0;
+}
